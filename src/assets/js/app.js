6c948960a457686ee8c6ce69e4933e0491490a9b
alexanderVLG <dominorog323@gmail.com>
2020-05-04 19:48:51 +0300

    AROS

---
+++ src/assets/js/app.js
@@ -0,0 +1,23 @@
+
+//../../../node_modules/materialize-css/js/modal.js
+
+
+
+//------ выпадающий список для переключения между страницами --------
+
+$('#switcher').on('click', function(){    
+  $('.submenu--hidden').slideToggle("");
+});
+
+$( document ).ready(function () {
+  $('.modal').modal({
+    opacity: 0.7,
+    inDuration: 800,
+    outDuration: 800,
+  });
+})
+
+
+
+
+
